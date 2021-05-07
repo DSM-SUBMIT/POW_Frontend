eaa44180e7bd9fd8ad50a34a13dd5415705f316b
Kimjimin0807 <jm004471@naver.com>
2021-05-08 03:54:27 +0900

    [ADD]<PictureUploadModal.js> 사진 업로드 모달 퍼블리싱

--- pow-front-v1/src/components/Club/modals/PictureUploadModal.js
+++ pow-front-v1/src/components/Club/modals/PictureUploadModal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as s from './style';
+import picturesmall from '../../IMG/picturesmall.png';
 
 const PictureUploadModal = () => {
   return(
@@ -9,6 +10,16 @@ const PictureUploadModal = () => {
         <s.Title>
           <p>사진 업로드</p>
         </s.Title>
+        <s.List>
+          <div>
+            <img src={picturesmall}></img>
+            <span>프로필 사진</span>
+          </div>
+          <div>
+            <img src={picturesmall}></img>
+            <span>배너 사진</span>
+          </div>
+        </s.List>
       </s.Modal>
     </>
   )
