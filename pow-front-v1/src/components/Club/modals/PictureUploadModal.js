8f072f1c7b8984d17059c935ba36c4576788b6ec
Kimjimin0807 <jm004471@naver.com>
2021-05-08 03:40:17 +0900

    [FEAT]<PictureUploadModal.js> 사진 업로드 모달 추가

---
+++ pow-front-v1/src/components/Club/modals/PictureUploadModal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import * as s from './style';
+
+const PictureUploadModal = () => {
+  return(
+    <>
+      <s.WhiteScreen></s.WhiteScreen>
+      <s.Modal>
+        <s.Title>
+          <p>사진 업로드</p>
+        </s.Title>
+      </s.Modal>
+    </>
+  )
+}
+
+export default PictureUploadModal;
