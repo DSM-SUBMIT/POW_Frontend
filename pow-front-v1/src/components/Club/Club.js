1ba1026b4e00ef594b33389b5706ff5924a8dc82
Kimjimin0807 <jm004471@naver.com>
2021-05-07 20:25:24 +0900

    [ADD]<Club.js> 클럽 페이지 퍼블리싱

--- pow-front-v1/src/components/Club/Club.js
+++ pow-front-v1/src/components/Club/Club.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import * as s from './style';
 import Banner from '../IMG/Banner.png';
+import logo from '../IMG/logo.png';
 
 const Club = () => {
   return(
     <>
-      <s.BannerImg>
-        <s.LogoDiv>
-          <span>.</span>
-        </s.LogoDiv>
-        <img src={Banner}></img>
-      </s.BannerImg>
+      <header>
+        <s.BannerImg>
+            <s.WhiteBox></s.WhiteBox>
+            <s.LogoDiv>
+                <img src={logo}></img>
+            </s.LogoDiv>
+            <img src={Banner}></img>
+          </s.BannerImg>
+      </header>
+      <section>
+        <s.LeftContent>
+          <s.ClubIntroBox>
+            <span>
+              Submit
+            </span>
+            <s.Writer>
+              작성자: 김지민
+            </s.Writer>
+            <s.FixDate>
+            수정일 : 2021-05-07
+            </s.FixDate>
+          </s.ClubIntroBox>
+        </s.LeftContent>
+      </section>
     </>
   );
 }
