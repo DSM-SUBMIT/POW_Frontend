8b20562525261d46599943679a1dd721a5022ea9
kky04994 <kkyo8747@naver.com>
2021-05-08 02:05:35 +0900

    [ADD] <PostModifyRemoveModal.js> Content code 추가

--- pow-front-v1/src/components/modal/PostModifyRemoveModal.js
+++ pow-front-v1/src/components/modal/PostModifyRemoveModal.js
@@ -1,1 +1,31 @@
 import React from "react";
+import * as S from "./style";
+import GreyBox from "./common/GreyBox";
+import * as F from "./common/SvgCollection";
+
+const PostModifyRemoveModal = () => {
+  const modalClose = () => {
+    return false;
+  };
+  return (
+    // <modal hidde={!show}>
+    <modal>
+      <GreyBox onClick={modalClose}>
+        <S.PmrContent>
+          <h4>게시글 수정 / 삭제</h4>
+          <S.Button>
+            <F.ModifySvg />
+            게시글 수정
+          </S.Button>
+          <S.Button>
+            <F.RemoveSvg />
+            게시글 삭제
+          </S.Button>
+          {}
+        </S.PmrContent>
+      </GreyBox>
+    </modal>
+  );
+};
+
+export default PostModifyRemoveModal;
