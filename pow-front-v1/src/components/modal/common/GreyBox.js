46fcd1fe12014e2370895172152b684eedb8815b
kky04994 <kkyo8747@naver.com>
2021-05-08 23:17:01 +0900

    [ADD] <GreyBox.js> 스타일 코드 추가

--- pow-front-v1/src/components/modal/common/GreyBox.js
+++ pow-front-v1/src/components/modal/common/GreyBox.js
@@ -1,1 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const GreyBox = styled.div`
+  width: 100%;
+  height: 100%;
+  background: #b9b9b9;
+  opacity: 0.5;
+  position: fixed;
+  display: flex;
+  left: 0;
+  top: 0;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default GreyBox;
