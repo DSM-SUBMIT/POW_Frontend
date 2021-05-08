e058a27a5cbd4db224f0ef38bca1509114892270
kky04994 <kkyo8747@naver.com>
2021-05-08 23:14:16 +0900

    [ADD] <App.js> 모달 코드 추가

--- pow-front-v1/src/App.js
+++ pow-front-v1/src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react";
+import ClubModifyModal from "./components/modal/ClubModifyModal";
+// import PostModifyModal from "./components/modal/PostModifyModal";
+// import PostModifyRemoveModal from "./components/modal/PostModifyRemoveModal";
+// import PostUploadModal from "./components/modal/PostUploadModal";
+// import ProjectIntroModal from "./components/modal/ProjectIntroModal";
+// import PostDelete from "./components/modal/PostDelete";
 
-function App() {
+const App = () => {
   return (
-    <p>das</p>
+    // <PostModifyRemoveModal />
+    // <PostDelete />
+    //<ClubModifyModal />
+    // <PostUploadModal />
+    // <PostModifyModal />
+    // <ProjectIntroModal />
   );
-}
+};
 
 export default App;
