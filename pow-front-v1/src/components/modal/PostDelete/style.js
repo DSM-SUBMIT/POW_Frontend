13438c104860e82da698100413f80dae4f08e59d
kky04994 <kkyo8747@naver.com>
2021-05-10 08:53:20 +0900

    [ADD] <style.js> 스타일 분리

---
+++ pow-front-v1/src/components/modal/PostDelete/style.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const DelContent = styled.div`
+  width: 620px;
+  height: 207px;
+  background: #ffffff;
+  border-radius: 23px;
+  z-index: 1;
+  border: 2px soild #000000;
+  text-align: center;
+  h4 {
+    font-family: NanumSquare_ac;
+  }
+`;
+
+export const CheckButton = styled.button`
+  width: 95px;
+  height: 59px;
+  background-color: #f4bea1;
+  border-radius: 17px;
+  border: none;
+  font-size: 19px;
+  color: #ffffff;
+  margin: 20px;
+`;
+
+export const NoButton = styled.button`
+  width: 95px;
+  height: 59px;
+  background-color: #bacff3;
+  border-radius: 17px;
+  border: none;
+  font-size: 19px;
+  color: #ffffff;
+  margin: 20px;
+`;
