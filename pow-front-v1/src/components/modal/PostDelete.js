fc7df1bd4959afecbf82295ff2985f719b2dbe6a
kky04994 <kkyo8747@naver.com>
2021-05-09 20:58:30 +0900

    [CHANGE] <PostDelete.js> Button 스타일 코드 바꿈

--- pow-front-v1/src/components/modal/PostDelete.js
+++ pow-front-v1/src/components/modal/PostDelete.js
@@ -2,18 +2,17 @@ import React from "react";
 import * as S from "./style";
 import GreyBox from "./common/GreyBox";
 
-const PostDeleteModal = () => {
+const PostDelete = () => {
   const modalClose = () => {
     return false;
   };
   return (
-    // <modal hidde={!show}>
     <modal>
       <GreyBox onClick={modalClose}>
         <S.DelContent>
-          <h4>게시글 수정 / 삭제</h4>
-          <S.Button>게시글 수정</S.Button>
-          <S.Button>게시글 삭제</S.Button>
+          <h4>게시글을 삭제합니다</h4>
+          <S.CheckButton>확인</S.CheckButton>
+          <S.NoButton>취소</S.NoButton>
         </S.DelContent>
       </GreyBox>
     </modal>
