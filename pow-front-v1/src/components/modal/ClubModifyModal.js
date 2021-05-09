4f30e697ce9215ccc7cac619447bcfe1f73d59f2
kky04994 <kkyo8747@naver.com>
2021-05-09 21:41:37 +0900

    [ADD] <ClubModifyModal.js> Content 스타일 코드 추가

--- pow-front-v1/src/components/modal/ClubModifyModal.js
+++ pow-front-v1/src/components/modal/ClubModifyModal.js
@@ -1,1 +1,24 @@
 import React from "react";
+import * as S from "./style";
+import GreyBox from "./common/GreyBox";
+
+const ClubModifyModal = () => {
+  return (
+    <modal>
+      <GreyBox>
+        <S.CmContent>
+          <h4>동아리 소개 수정</h4>
+          <S.InputText placeholder="작성자를 입력해 주세요" />
+          <div>
+            <S.FileLabel for="file">파일 선택</S.FileLabel>
+            <S.FileInput type="file" id="file" />
+            <S.Uploadname placeholder="파일 선택"></S.Uploadname>
+          </div>
+          <S.UploadButton>업로드 하기</S.UploadButton>
+        </S.CmContent>
+      </GreyBox>
+    </modal>
+  );
+};
+
+export default ClubModifyModal;
