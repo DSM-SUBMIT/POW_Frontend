7b4c0bd11774a1f84711856c3bb42dc7eb5a2422
kky04994 <kkyo8747@naver.com>
2021-05-10 09:31:17 +0900

    [ADD] <style.js> 스타일 분리 후 스타일 코드 추가

---
+++ pow-front-v1/src/components/modal/PostModifyRemove/style.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const PmrContent = styled.div`
+  width: 620px;
+  height: 207px;
+  background: #ffffff;
+  border-radius: 23px;
+  z-index: 1;
+  border: 2px soild #000000;
+  text-align: center;
+  h4 {
+    font-family: NanumSquare_ac;
+  }
+`;
+
+export const Button = styled.button`
+  width: 180px;
+  height: 59px;
+  background: #f4f4f4;
+  border-radius: 17px;
+  border: none;
+  margin: 18px;
+  font-size: 19px;
+  line-height: 49px;
+  font-family: NanumSquare_ac;
+  svg {
+    vertical-align: middle;
+    margin-right: 9px;
+    margin-bottom: 4px;
+  }
+`;
