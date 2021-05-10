fc49f272dda5362efdac4612343b3c3c250f8a19
kky04994 <kkyo8747@naver.com>
2021-05-10 09:20:55 +0900

    [REFACTOR] <PostUploadModal.js> 폴더 구조 변경

---
+++ pow-front-v1/src/components/modal/PostUpload/PostUploadModal.js
@@ -0,0 +1,30 @@
+import React from "react";
+import * as S from "../PostUpload/style";
+import GreyBox from "../common/GreyBox";
+
+const PostUploadModal = () => {
+  return (
+    <modal>
+      <GreyBox>
+        <S.UpContent>
+          <h4>게시물 올리기</h4>
+          <S.InputText placeholder="제목을 입력해 주세요" />
+          <S.DayBox>
+            <S.StartLable>시작일 : </S.StartLable>
+            <S.InputDay />
+            <S.StartLable>종료일 : </S.StartLable>
+            <S.InputDay />
+          </S.DayBox>
+          <div>
+            <S.FileLabel for="file">파일 선택</S.FileLabel>
+            <S.FileInput type="file" id="file" />
+            <S.Uploadname placeholder="파일 선택" />
+          </div>
+          <S.UploadButton>업로드 하기</S.UploadButton>
+        </S.UpContent>
+      </GreyBox>
+    </modal>
+  );
+};
+
+export default PostUploadModal;
