4a57d4f152339c5075c002dbdd5092628cc742b5
kky04994 <kkyo8747@naver.com>
2021-05-11 16:01:51 +0900

    [ADD] <PostDelete.js> Title Line 코드 추가

--- pow-front-v1/src/components/modal/PostDelete/PostDelete.js
+++ pow-front-v1/src/components/modal/PostDelete/PostDelete.js
@@ -7,7 +7,9 @@ const PostDelete = () => {
     <modal>
       <GreyBox>
         <S.DelContent>
-          <h4>게시글을 삭제합니다</h4>
+          <S.Title>
+            <p>게시글을 삭제합니다</p>
+          </S.Title>
           <S.CheckButton>확인</S.CheckButton>
           <S.NoButton>취소</S.NoButton>
         </S.DelContent>
