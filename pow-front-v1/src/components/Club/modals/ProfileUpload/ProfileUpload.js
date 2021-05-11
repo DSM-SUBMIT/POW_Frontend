7d8bb6572b16f68ff238a09c241bb2dc838de19e
Kimjimin0807 <jm004471@naver.com>
2021-05-11 23:19:07 +0900

    [ADD]<ProfileUpload.js>프로필 업로드 모달 추가

--- pow-front-v1/src/components/Club/modals/ProfileUpload/ProfileUpload.js
+++ pow-front-v1/src/components/Club/modals/ProfileUpload/ProfileUpload.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as s from './style';
 
-const ProfileUpload = () => {
+const ProfileUpload = (props) => {
+  const [filePath, setFilePath] = useState('');
+
+  const onClickWhiteScreen = () => {
+    props.setProfileModal(false);
+  }
+
+  const onChangeFile = (e) => {
+    console.log("asdas");
+    console.log(e.target.files[0]);
+    setFilePath(e.target.value);
+  }
+
   return(
     <>
-      <s.WhiteScreen></s.WhiteScreen>
+      <s.WhiteScreen onClick={onClickWhiteScreen}></s.WhiteScreen>
       <s.Modal>
         <s.Title>
           <p>프로필 사진 업로드</p>
         </s.Title>
         <s.FileInput>
-          <s.FileBtn>파일 선택</s.FileBtn>
-          <s.FilePath>Submit.jpg</s.FilePath>
+          <s.FileBtn htmlFor="fileUpload" ><p>파일 선택</p></s.FileBtn>
+          <s.FilePath htmlFor="fileUpload"><p>{filePath}</p></s.FilePath>
+          <s.FileUpload id="fileUpload" type="file" onChange={onChangeFile}></s.FileUpload>
         </s.FileInput>
         <s.UploadBtn>업로드 하기</s.UploadBtn>
       </s.Modal>
