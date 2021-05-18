6e2007c1829b593354f4d332abf9692fac395dcd
Kimjimin0807 <jm004471@naver.com>
2021-05-18 17:04:38 +0900

    [ADD]<BannerUpload.js> 파일 업로드 추가

--- pow-front-v1/src/components/Club/modals/BannerUpload/BannerUpload.js
+++ pow-front-v1/src/components/Club/modals/BannerUpload/BannerUpload.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as s from './style';
 
 const BannerUpload = (props) => {
-  
+  const [filePath, setFilePath] = useState('');
+
   const onClickWhiteScreen = () => {
     props.setBannerModal(false);
+  }
+
+  const onChangeFile = (e) => {
+    console.log("asdas");
+    console.log(e.target.files[0]);
+    let str = e.target.value.slice(12, e.target.value.length);
+    setFilePath(str);
   }
 
   return(
@@ -15,8 +23,9 @@ const BannerUpload = (props) => {
           <p>배너 사진 업로드</p>
         </s.Title>
         <s.FileInput>
-          <s.FileBtn>파일 선택</s.FileBtn>
-          <s.FilePath>Submit.jpg</s.FilePath>
+          <s.FileBtn htmlFor="fileUpload" ><p>파일 선택</p></s.FileBtn>
+          <s.FilePath htmlFor="fileUpload"><p>{filePath}</p></s.FilePath>
+          <s.FileUpload id="fileUpload" type="file" onChange={onChangeFile}></s.FileUpload>
         </s.FileInput>
         <s.UploadBtn>업로드 하기</s.UploadBtn>
       </s.Modal>
