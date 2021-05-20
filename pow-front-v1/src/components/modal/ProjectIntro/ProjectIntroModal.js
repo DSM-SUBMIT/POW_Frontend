dc3f32a0cf761d873022bfae65d4998467a4edde
kky04994 <kkyo8747@naver.com>
2021-05-20 23:46:18 +0900

    [ADD] <ProjectIntroModal.js> Content 기능 추가

--- pow-front-v1/src/components/modal/ProjectIntro/ProjectIntroModal.js
+++ pow-front-v1/src/components/modal/ProjectIntro/ProjectIntroModal.js
@@ -6,11 +6,9 @@ const ProjectlntroModal = () => {
   return (
     <modal>
       <GreyBox>
-        <S.DelContent>
-          <h4>게시글 수정 / 삭제</h4>
-          <S.Button>게시글 수정</S.Button>
-          <S.Button>게시글 삭제</S.Button>
-        </S.DelContent>
+        <S.IntroContent>
+          <S.Content></S.Content>
+        </S.IntroContent>
       </GreyBox>
     </modal>
   );
