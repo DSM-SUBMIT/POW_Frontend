79e6aa45d2a27f753f074f98dab9923373acc15f
kky04994 <kkyo8747@naver.com>
2021-05-20 23:46:38 +0900

    [ADD] <style.js> Content 스타일 추가

--- pow-front-v1/src/components/modal/ProjectIntro/style.js
+++ pow-front-v1/src/components/modal/ProjectIntro/style.js
@@ -1,3 +1,30 @@
 import styled from "styled-components";
 
-export const IntroContent = styled.div``;
+export const IntroContent = styled.div`
+  width: 1188px;
+  height: 718px;
+  background: #ffffff;
+  border-radius: 27px;
+  z-index: 1;
+  border: 2px soild #000000;
+  text-align: center;
+`;
+
+export const Content = styled.div`
+  margin-top: 50px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  height: 700px;
+  width: 1100px;
+  ::-webkit-scrollbar {
+    width: 15px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: #9f91d6;
+    border-radius: 10px;
+  }
+  ::-webkit-scrollbar-track {
+    background-color: white;
+    border-radius: 10px;
+  }
+`;
