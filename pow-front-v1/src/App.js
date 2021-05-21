dbd4213e10cb851c1970278cb2fc42aa0dd13c56
kky04994 <kkyo8747@naver.com>
2021-05-21 20:26:58 +0900

    [STYLE] <App.js> ProjectIntroModal 빼고 주석처리

--- pow-front-v1/src/App.js
+++ pow-front-v1/src/App.js
@@ -1,19 +1,19 @@
 import react from "react";
-import ClubModifyModal from "./components/modal/ClubModifyModal";
-// import PostModifyModal from "./components/modal/PostModifyModal";
-// import PostModifyRemoveModal from "./components/modal/PostModifyRemoveModal";
-// import PostUploadModal from "./components/modal/PostUploadModal";
-// import ProjectIntroModal from "./components/modal/ProjectIntroModal";
-// import PostDelete from "./components/modal/PostDelete";
+// import ClubModifyModal from "./components/modal/ClubModfiy/ClubModifyModal";
+// import PostModifyModal from "./components/modal/PostModify/PostModifyModal";
+// import PostModifyRemoveModal from "./components/modal/PostModifyRemove/PostModifyRemoveModal";
+// import PostUploadModal from "./components/modal/PostUpload/PostUploadModal";
+import ProjectIntroModal from "./components/modal/ProjectIntro/ProjectIntroModal";
+// import PostDelete from "./components/modal/PostDelete/PostDelete";
 
 const App = () => {
   return (
-    // <PostModifyRemoveModal />
+    //<PostModifyRemoveModal />
     // <PostDelete />
     //<ClubModifyModal />
     // <PostUploadModal />
     // <PostModifyModal />
-    // <ProjectIntroModal />
+    <ProjectIntroModal />
   );
 };
 
