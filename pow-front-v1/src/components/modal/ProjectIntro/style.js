502d9eaf384503b2d442c0a0870bced524993b7f
kky04994 <kkyo8747@naver.com>
2021-05-22 02:14:40 +0900

    [ADD] <style.js> Header와 Box 각각 스타일 코드 추가

--- pow-front-v1/src/components/modal/ProjectIntro/style.js
+++ pow-front-v1/src/components/modal/ProjectIntro/style.js
@@ -10,38 +10,69 @@ export const IntroContent = styled.div`
   text-align: center;
 `;
 
-export const Content = styled.div`
-  margin-top: 50px;
-  overflow-y: scroll;
-  overflow-x: hidden;
-  height: 700px;
-  width: 1100px;
-  ::-webkit-scrollbar {
-    width: 15px;
-  }
-  Content::-webkit-scrollbar-thumb {
-    background-color: #9f91d6;
-    border-radius: 10px;
-  }
-  Content::-webkit-scrollbar-track {
-    background-color: white;
-    border-radius: 10px;
-  }
+export const HeaderContent = styled.div`
+  width: 1054px;
+  height: 116px;
+  padding-top: 38px;
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
 `;
 
-export const HeaderContent = styled.div`
-  width: 80%;
-`;
 export const WriteBar = styled.div`
   width: 80%;
+  height: 61px;
+  p {
+    width: 178px;
+    display: inline;
+    font-size: 20px;
+    align: left;
+  }
 `;
 
 export const TitleBar = styled.div`
   width: 80%;
+  height: 61px;
+  margin-bottom: 27px;
+  h3 {
+    width: 656px;
+    display: inline;
+    margin-right: 40px;
+  }
+  p {
+    width: 178px;
+    display: inline;
+    font-size: 20px;
+  }
 `;
 
 export const IntroduceBox = styled.div`
   width: 1054px;
   height: 470px;
-  background-color: blue;
+  background: #f3f3f3;
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%);
+  overflow-y: scroll;
+  overflow-x: hidden;
+  ::-webkit-scrollbar {
+    width: 15px;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: #red;
+  }
+  ::-webkit-scrollbar-track {
+    background-color: #7662c6;
+    border-radius: 10px;
+  }
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+  ::-webkit-scrollbar-track {
+    background-color: #f9f9f9;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: #7662c6;
+    border-radius: 30px;
+  }
 `;
