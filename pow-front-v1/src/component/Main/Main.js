6e692073d706cb6ba6498034cd64231070fdb041
yelim <choyr2016@naver.com>
2021-05-21 16:53:54 +0900

    [FEAT] <Main.js>

---
+++ pow-front-v1/src/component/Main/Main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import * as s from './style';
+
+const Main = () => {
+    return(
+        <s.main>
+            <s.line>
+                <s.club></s.club>
+                <s.club></s.club>
+                <s.club></s.club>
+                <s.club></s.club>
+            </s.line>
+            <s.line>
+                <s.club></s.club>
+                <s.club></s.club>
+                <s.club></s.club>
+                <s.club></s.club>
+            </s.line>
+        </s.main>
+    )
+}
+
+export default Main;
