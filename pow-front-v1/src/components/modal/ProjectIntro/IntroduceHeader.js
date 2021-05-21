657a2eb3ded72b494d1edd8b1da63d55f337dd81
kky04994 <kkyo8747@naver.com>
2021-05-21 19:57:09 +0900

    [FEAT] <IntroduceHeader.js> Header 기능 추가

---
+++ pow-front-v1/src/components/modal/ProjectIntro/IntroduceHeader.js
@@ -0,0 +1,18 @@
+import React from "react";
+import * as S from "../ProjectIntro/style";
+
+const IntroduceHeader = () => {
+    return (
+    <S.WriteBar>
+        <p>작성일 : 2021-05-07</p>
+        <p>수정일 : 2021-05-21</p>
+        <p>작성자 : 김해교</p>
+    </S.WriteBar>
+    <S.TitleBar>
+        <h3>제목입니다</h3>
+        <p>프로젝트 시작일 : 21-05-07 ~ 21-05-30</p>
+    </S.TitleBar>
+    );
+  };
+
+export default IntroduceHeader;
