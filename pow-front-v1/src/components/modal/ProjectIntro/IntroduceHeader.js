950928eca36761dafc1d44287474c9f566ba4f87
kky04994 <kkyo8747@naver.com>
2021-05-21 20:20:18 +0900

    [ADD] <IntroduceHeader.js> Content로 묶음

--- pow-front-v1/src/components/modal/ProjectIntro/IntroduceHeader.js
+++ pow-front-v1/src/components/modal/ProjectIntro/IntroduceHeader.js
@@ -2,17 +2,19 @@ import React from "react";
 import * as S from "../ProjectIntro/style";
 
 const IntroduceHeader = () => {
-    return (
-    <S.WriteBar>
+  return (
+    <S.HeaderContent>
+      <S.WriteBar>
         <p>작성일 : 2021-05-07</p>
         <p>수정일 : 2021-05-21</p>
         <p>작성자 : 김해교</p>
-    </S.WriteBar>
-    <S.TitleBar>
+      </S.WriteBar>
+      <S.TitleBar>
         <h3>제목입니다</h3>
         <p>프로젝트 시작일 : 21-05-07 ~ 21-05-30</p>
-    </S.TitleBar>
-    );
-  };
+      </S.TitleBar>
+    </S.HeaderContent>
+  );
+};
 
 export default IntroduceHeader;
