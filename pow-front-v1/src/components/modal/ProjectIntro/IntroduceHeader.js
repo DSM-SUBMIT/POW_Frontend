d350e37211068b1b819aaf576f40b401e23628bd
kky04994 <kkyo8747@naver.com>
2021-05-22 03:36:14 +0900

    [CHANGE] <IntroduceHeader.js> 컴포던트로 묶음

--- pow-front-v1/src/components/modal/ProjectIntro/IntroduceHeader.js
+++ pow-front-v1/src/components/modal/ProjectIntro/IntroduceHeader.js
@@ -5,13 +5,13 @@ const IntroduceHeader = () => {
   return (
     <S.HeaderContent>
       <S.WriteBar>
-        <p>작성일 : 2021-05-07</p>
-        <p>수정일 : 2021-05-21</p>
-        <p>작성자 : 김해교</p>
+        <S.CreatedDate>작성일 : 2021-05-07</S.CreatedDate>
+        <S.ModifiedDate>수정일 : 2021-05-21</S.ModifiedDate>
+        <S.Writer>작성자 : 김해교</S.Writer>
       </S.WriteBar>
       <S.TitleBar>
-        <h3>제목입니다</h3>
-        <p>프로젝트 시작일 : 21-05-07 ~ 21-05-30</p>
+        <S.IntroduceTitle>제목입니다.</S.IntroduceTitle>
+        <S.StartDate>프로젝트 시작일 : 21-05-07 ~ 21-05-30</S.StartDate>
       </S.TitleBar>
     </S.HeaderContent>
   );
