c2cfa53839bd777807bcb556297300a8f871d987
yelim <choyr2016@naver.com>
2021-05-23 19:26:06 +0900

    [CHANGE] <App.js>

--- pow-front-v1/src/App.js
+++ pow-front-v1/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import react from "react";
 // import ClubModifyModal from "./components/modal/ClubModfiy/ClubModifyModal";
 // import PostModifyModal from "./components/modal/PostModify/PostModifyModal";
 // import PostModifyRemoveModal from "./components/modal/PostModifyRemove/PostModifyRemoveModal";
 // import PostUploadModal from "./components/modal/PostUpload/PostUploadModal";
 import ProjectIntroModal from "./components/modal/ProjectIntro/ProjectIntroModal";
 // import PostDelete from "./components/modal/PostDelete/PostDelete";
-import React from "react";
 import Club from "./components/Club/Club";
 import Header from "./components/Header/Header";
 import Main from './component/Main/Main';
@@ -15,10 +13,10 @@ import "./App.css";
 const App = () => {
   return (
     <>
-      <Header></Header>
+      {/* <Header></Header> */}
       <Main></Main>
-      <Club></Club>
-      <ProjectIntroModal />
+      {/* <Club></Club> */}
+      {/* <ProjectIntroModal /> */}
     </>
   );
 };
