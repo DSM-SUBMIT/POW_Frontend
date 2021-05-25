aef0627738efe5dc5619e499dc062dc9ca456369
Kimjimin0807 <jm004471@naver.com>
2021-05-25 23:04:48 +0900

    [CHANGE]<Club.js>디자인 변경

--- pow-front-v1/src/components/Club/Club.js
+++ pow-front-v1/src/components/Club/Club.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as s from './style';
 import Banner from '../IMG/Banner.png';
 import logo from '../IMG/logo.png';
@@ -9,6 +9,7 @@ import list from '../IMG/list.png';
 import PictureUploadModal from './modals/pictureUploadModal/PictureUploadModal';
 import ProfileUpload from './modals/ProfileUpload/ProfileUpload';
 import BannerUpload from './modals/BannerUpload/BannerUpload';
+import { FileRequest } from '../Axios/Axios';
 
 const Club = () => {
   const [pictureModal, setPictureModal] = useState(false);
@@ -41,82 +42,83 @@ const Club = () => {
             <s.LogoDiv>
                 <img src={logo}></img>
             </s.LogoDiv>
-            <img src={Banner}></img>
+            <img src='https://ehddkfl.herokuapp.com/public/images/DefaultImage.png'></img>
         </s.BannerImg>
       </header>
-      <section>
-        <s.LeftContent>
-          <s.ClubIntroBox>
-            <span>
-              Submit
-            </span>
-            <s.Writer>
-              작성자: 김지민
-            </s.Writer>
-            <s.FixDate>
-            수정일 : 2021-05-07
-            </s.FixDate>
-          </s.ClubIntroBox>
-        </s.LeftContent>
-        <s.RightContent>
-          <s.Upload>
-            <s.PictureUpload onClick = {onClickPictureModal}>
-              <img src={picture}></img>
-              <span>사진 업로드</span>
-            </s.PictureUpload>
-            <s.ClubFix>
-              <img src={edit}></img>
-              <span>동아리 소개 수정</span>
-            </s.ClubFix>
-            <s.PostUpload>
-              <img src={writing}></img>
-              <span>게시물 업로드</span>
-            </s.PostUpload>
-          </s.Upload>
-          <s.Content>
-            <s.Post>
-              <img src={list}></img>
-              <s.PostDiv>
-                <p>작성일 : 2021-04-10</p>
-                <p>수정일 : 2021-04-11</p>
-                <p>작성자 : 김해교</p>
-              </s.PostDiv>
-              <s.Title>제목입니다.</s.Title>
-              <s.StartDate>프로젝트 시작일 : 21-03-02 ~ 21-06-30</s.StartDate>
-            </s.Post>
-            <s.Post>
-              <img src={list}></img>
-              <s.PostDiv>
-                <p>작성일 : 2021-04-10</p>
-                <p>수정일 : 2021-04-11</p>
-                <p>작성자 : 김해교</p>
-              </s.PostDiv>
-              <s.Title>제목입니다.</s.Title>
-              <s.StartDate>프로젝트 시작일 : 21-03-02 ~ 21-06-30</s.StartDate>
-            </s.Post>
-            <s.Post>
-              <img src={list}></img>
-              <s.PostDiv>
-                <p>작성일 : 2021-04-10</p>
-                <p>수정일 : 2021-04-11</p>
-                <p>작성자 : 김해교</p>
-              </s.PostDiv>
-              <s.Title>제목입니다.</s.Title>
-              <s.StartDate>프로젝트 시작일 : 21-03-02 ~ 21-06-30</s.StartDate>
-            </s.Post>
-            <s.Post>
-              <img src={list}></img>
-              <s.PostDiv>
-                <p>작성일 : 2021-04-10</p>
-                <p>수정일 : 2021-04-11</p>
-                <p>작성자 : 김해교</p>
-              </s.PostDiv>
-              <s.Title>제목입니다.</s.Title>
-              <s.StartDate>프로젝트 시작일 : 21-03-02 ~ 21-06-30</s.StartDate>
-            </s.Post>
-          </s.Content>
-        </s.RightContent>
-        
+      <section style={{"backgroundColor": "#FCFCFC"}}>
+        <s.MainContent>
+          <s.LeftContent>
+            <s.ClubIntroBox>
+              <span>
+                Submit
+              </span>
+              <s.Writer>
+                작성자: 김지민
+              </s.Writer>
+              <s.FixDate>
+              수정일 : 2021-05-07
+              </s.FixDate>
+            </s.ClubIntroBox>
+          </s.LeftContent>
+          <s.RightContent>
+            <s.Upload>
+              <s.PictureUpload onClick = {onClickPictureModal}>
+                <img src={picture}></img>
+                <span>사진 업로드</span>
+              </s.PictureUpload>
+              <s.ClubFix>
+                <img src={edit}></img>
+                <span>동아리 소개 수정</span>
+              </s.ClubFix>
+              <s.PostUpload>
+                <img src={writing}></img>
+                <span>게시물 업로드</span>
+              </s.PostUpload>
+            </s.Upload>
+            <s.Content>
+              <s.Post>
+                <img src={list}></img>
+                <s.PostDiv>
+                  <p>작성일 : 2021-04-10</p>
+                  <p>수정일 : 2021-04-11</p>
+                  <p>작성자 : 김해교</p>
+                </s.PostDiv>
+                <s.Title>제목입니다.</s.Title>
+                <s.StartDate>프로젝트 시작일 : 21-03-02 ~ 21-06-30</s.StartDate>
+              </s.Post>
+              <s.Post>
+                <img src={list}></img>
+                <s.PostDiv>
+                  <p>작성일 : 2021-04-10</p>
+                  <p>수정일 : 2021-04-11</p>
+                  <p>작성자 : 김해교</p>
+                </s.PostDiv>
+                <s.Title>제목입니다.</s.Title>
+                <s.StartDate>프로젝트 시작일 : 21-03-02 ~ 21-06-30</s.StartDate>
+              </s.Post>
+              <s.Post>
+                <img src={list}></img>
+                <s.PostDiv>
+                  <p>작성일 : 2021-04-10</p>
+                  <p>수정일 : 2021-04-11</p>
+                  <p>작성자 : 김해교</p>
+                </s.PostDiv>
+                <s.Title>제목입니다.</s.Title>
+                <s.StartDate>프로젝트 시작일 : 21-03-02 ~ 21-06-30</s.StartDate>
+              </s.Post>
+              <s.Post>
+                <img src={list}></img>
+                <s.PostDiv>
+                  <p>작성일 : 2021-04-10</p>
+                  <p>수정일 : 2021-04-11</p>
+                  <p>작성자 : 김해교</p>
+                </s.PostDiv>
+                <s.Title>제목입니다.</s.Title>
+                <s.StartDate>프로젝트 시작일 : 21-03-02 ~ 21-06-30</s.StartDate>
+              </s.Post>
+            </s.Content>
+          </s.RightContent>
+        </s.MainContent>
       </section>
     </>
   );
