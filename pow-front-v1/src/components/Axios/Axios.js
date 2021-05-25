3cee3493ed0a7f845538f31d0efd16a9c35e8002
Kimjimin0807 <jm004471@naver.com>
2021-05-25 23:04:31 +0900

    [DOCS]<Axios.js> 문서이름 변경

--- pow-front-v1/src/components/Axios/Axios.js
+++ pow-front-v1/src/components/Axios/Axios.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const baseURL = 'https://ehddkfl.herokuapp.com/';
+const fileURL = 'https://ehddkfl.herokuapp.com/';
 
 export const FileRequest = async(method, url, head, data) => {
   return await axios({
     method: method,
-    url: baseURL + url,
+    url: 'https://ehddkfl.herokuapp.com/public/images/DefaultImage.png',
     headers: head,
     data: data
   }).then((e)=>{
