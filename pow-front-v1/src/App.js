e8cd6362b205d7d590b7ffb88d4fdca2743ff873
kky04994 <kkyo8747@naver.com>
2021-05-25 20:13:54 +0900

    [CHANGE] <App.js> 주석 처리

--- pow-front-v1/src/App.js
+++ pow-front-v1/src/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
+import React from "react";
 // import ClubModifyModal from "./components/modal/ClubModfiy/ClubModifyModal";
 // import PostModifyModal from "./components/modal/PostModify/PostModifyModal";
 // import PostModifyRemoveModal from "./components/modal/PostModifyRemove/PostModifyRemoveModal";
 // import PostUploadModal from "./components/modal/PostUpload/PostUploadModal";
-import ProjectIntroModal from "./components/modal/ProjectIntro/ProjectIntroModal";
 // import PostDelete from "./components/modal/PostDelete/PostDelete";
 import Club from "./components/Club/Club";
 import Header from "./components/Header/Header";
-import Main from './component/Main/Main';
+import Main from "./component/Main/Main";
 import "./App.css";
 
 const App = () => {
   return (
     <>
-      {/* <Header></Header> */}
-      <Main></Main>
-      {/* <Club></Club> */}
-      {/* <ProjectIntroModal /> */}
+      <Header></Header>
+      {/*<Main></Main> */}
+      <Club></Club>
     </>
   );
 };
