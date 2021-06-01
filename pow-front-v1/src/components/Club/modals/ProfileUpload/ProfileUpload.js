22d2e6f22f8e2b7643d1b07ee8847d4540267861
kky04994 <kkyo8747@naver.com>
2021-06-01 21:54:11 +0900

    !

--- pow-front-v1/src/components/Club/modals/ProfileUpload/ProfileUpload.js
+++ pow-front-v1/src/components/Club/modals/ProfileUpload/ProfileUpload.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import * as s from './style';
+import React, { useState } from "react";
+import * as s from "./style";
 
 const ProfileUpload = (props) => {
-  const [filePath, setFilePath] = useState('');
+  const [filePath, setFilePath] = useState("");
 
   const onClickWhiteScreen = () => {
     props.setProfileModal(false);
-  }
+  };
 
   const onChangeFile = (e) => {
     console.log(e.target.files[0]);
     setFilePath(e.target.value);
-  }
+  };
 
-  return(
+  return (
     <>
       <s.WhiteScreen onClick={onClickWhiteScreen}></s.WhiteScreen>
       <s.Modal>
@@ -21,14 +21,22 @@ const ProfileUpload = (props) => {
           <p>프로필 사진 업로드</p>
         </s.Title>
         <s.FileInput>
-          <s.FileBtn htmlFor="fileUpload" ><p>파일 선택</p></s.FileBtn>
-          <s.FilePath htmlFor="fileUpload"><p>{filePath}</p></s.FilePath>
-          <s.FileUpload id="fileUpload" type="file" onChange={onChangeFile}></s.FileUpload>
+          <s.FileBtn htmlFor="fileUpload">
+            <p>파일 선택</p>
+          </s.FileBtn>
+          <s.FilePath htmlFor="fileUpload">
+            <p>{filePath}</p>
+          </s.FilePath>
+          <s.FileUpload
+            id="fileUpload"
+            type="file"
+            onChange={onChangeFile}
+          ></s.FileUpload>
         </s.FileInput>
         <s.UploadBtn>업로드 하기</s.UploadBtn>
       </s.Modal>
     </>
-  )
-}
+  );
+};
 
 export default ProfileUpload;
