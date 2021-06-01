966b2ed1beb476ff742889e76f2bfb5ad5796053
kky04994 <kkyo8747@naver.com>
2021-06-01 22:57:28 +0900

    [REMOVE] <App.js> 주석으로 된 코드 삭제

--- pow-front-v1/src/App.js
+++ pow-front-v1/src/App.js
@@ -1,9 +1,4 @@
 import React from "react";
-// import ClubModifyModal from "./components/modal/ClubModfiy/ClubModifyModal";
-// import PostModifyModal from "./components/modal/PostModify/PostModifyModal";
-// import PostModifyRemoveModal from "./components/modal/PostModifyRemove/PostModifyRemoveModal";
-// import PostUploadModal from "./components/modal/PostUpload/PostUploadModal";
-// import PostDelete from "./components/modal/PostDelete/PostDelete";
 import Club from "./components/Club/Club";
 import Header from "./components/Header/Header";
 import "./App.css";
