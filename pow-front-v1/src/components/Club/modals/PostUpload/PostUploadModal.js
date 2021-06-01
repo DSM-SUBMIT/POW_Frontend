552f5040375b90eadb5cfc53b5b9eac8e276ebc2
kky04994 <kkyo8747@naver.com>
2021-06-01 18:33:02 +0900

    [ADD] <PostUploadModal.js> react-DatePicker 기능 추가

--- pow-front-v1/src/components/Club/modals/PostUpload/PostUploadModal.js
+++ pow-front-v1/src/components/Club/modals/PostUpload/PostUploadModal.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "../PostUpload/style";
 import GreyBox from "../common/GreyBox";
+import "react-datepicker/dist/react-datepicker.css";
 
 const PostUploadModal = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
   return (
     <modal>
       <GreyBox>
@@ -13,10 +16,21 @@ const PostUploadModal = () => {
           <S.UploadHeader>
             <S.InputText placeholder="제목을 입력해 주세요" />
             <S.DayBox>
-              <S.StartLable>시작일 : </S.StartLable>
-              <S.InputDay />
-              <S.StartLable>종료일 : </S.StartLable>
-              <S.InputDay />
+              <S.DayLable>시작일 : </S.DayLable>
+              <S.SelectDate
+                selected={startDate}
+                dateFormat="yyyy-MM-dd"
+                onChange={(date) => setStartDate(date)}
+                placeholder="클릭해 주세요."
+              />
+              <S.DayLable>종료일 : </S.DayLable>
+              <S.SelectDate
+                selected={endDate}
+                dateFormat="yyyy-MM-dd"
+                onChange={(date) => setEndDate(date)}
+                minDate={startDate}
+                placeholder="클릭해 주세요."
+              />
             </S.DayBox>
           </S.UploadHeader>
           <S.WriteBox>
