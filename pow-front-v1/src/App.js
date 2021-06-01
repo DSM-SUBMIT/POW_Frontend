fd0b87a060611e1b9a5aecfdca90b5b6d9058229
kky04994 <kkyo8747@naver.com>
2021-06-01 18:29:57 +0900

    [REMOVE] <App.js> 주석 처리된 코드들 삭제

--- pow-front-v1/src/App.js
+++ pow-front-v1/src/App.js
@@ -1,21 +1,12 @@
-import react from "react";
-// import ClubModifyModal from "./components/modal/ClubModfiy/ClubModifyModal";
-// import PostModifyModal from "./components/modal/PostModify/PostModifyModal";
-// import PostModifyRemoveModal from "./components/modal/PostModifyRemove/PostModifyRemoveModal";
-// import PostUploadModal from "./components/modal/PostUpload/PostUploadModal";
-import ProjectIntroModal from "./components/modal/ProjectIntro/ProjectIntroModal";
-// import PostDelete from "./components/modal/PostDelete/PostDelete";
 import React from "react";
-import Club from "./components/Club/Club";
-import Header from "./components/Header/Header";
-import "./App.css";
+// import Club from "./components/Club/Club";
+// import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <>
-      <Header></Header>
-      <Club></Club>
-      <ProjectIntroModal />
+      {/*b<Header></Header> */}
+      {/* <Club></Club> */}
     </>
   );
 };
