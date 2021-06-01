0bedc62166af1b0153a1eec05a61dd64ee60783a
yelim <choyr2016@naver.com>
2021-06-01 19:10:11 +0900

    [MODIFY] 코드 수정 후 다시 커밋

--- pow-front-v1/src/App.js
+++ pow-front-v1/src/App.js
@@ -13,7 +13,7 @@ import "./App.css";
 const App = () => {
   return (
     <>
-      {/* <Header></Header> */}
+      <Header></Header>
       <Main></Main>
       {/* <Club></Club> */}
       {/* <ProjectIntroModal /> */}
