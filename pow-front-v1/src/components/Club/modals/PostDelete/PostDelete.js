7a5f750ef954d63e8201308917e2692ad6ff8193
kky04994 <kkyo8747@naver.com>
2021-06-02 20:03:26 +0900

    [ADD] <PostDelete.js> WhiteScreen 추가 후 모달 기능 추가

--- pow-front-v1/src/components/Club/modals/PostDelete/PostDelete.js
+++ pow-front-v1/src/components/Club/modals/PostDelete/PostDelete.js
@@ -1,19 +1,21 @@
 import React from "react";
 import * as S from "../PostDelete/style";
-import GreyBox from "../common/GreyBox";
+import WhiteScreen from "../common/WhiteScreen";
 
-const PostDelete = () => {
+const PostDelete = (props) => {
+  const onClickPostDelete = () => {
+    props.setPostDeleteModal(false);
+  };
   return (
     <modal>
-      <GreyBox>
-        <S.DelContent>
-          <S.Title>
-            <p>게시글을 삭제합니다</p>
-          </S.Title>
-          <S.CheckButton>확인</S.CheckButton>
-          <S.NoButton>취소</S.NoButton>
-        </S.DelContent>
-      </GreyBox>
+      <WhiteScreen onClick={onClickPostDelete} />
+      <S.DelContent>
+        <S.Title>
+          <p>게시글을 삭제합니다</p>
+        </S.Title>
+        <S.CheckButton>확인</S.CheckButton>
+        <S.NoButton>취소</S.NoButton>
+      </S.DelContent>
     </modal>
   );
 };
