9d8c8a71f1854aa3f7907d3339a9a4f46c6ff267
kky04994 <kkyo8747@naver.com>
2021-06-02 19:58:45 +0900

    [ADD] <ProjectIntroModal.js> WhiteScreen 추가 후 모달 기능 추가

--- pow-front-v1/src/components/Club/modals/ProjectIntro/ProjectIntroModal.js
+++ pow-front-v1/src/components/Club/modals/ProjectIntro/ProjectIntroModal.js
@@ -1,19 +1,21 @@
 import React from "react";
 import * as S from "../ProjectIntro/style";
-import GreyBox from "../common/GreyBox";
+import WhiteScreen from "../common/WhiteScreen";
 import IntroduceHeader from "./IntroduceHeader";
 
-const ProjectlntroModal = () => {
+const ProjectlntroModal = (props) => {
+  const onClickProjectIntroModal = () => {
+    props.setProjectIntroModal(false);
+  };
   return (
     <modal>
-      <GreyBox>
-        <S.IntroContent>
-          <IntroduceHeader />
-          <S.IntroduceBox>
-            <p>.</p>
-          </S.IntroduceBox>
-        </S.IntroContent>
-      </GreyBox>
+      <WhiteScreen onClick={onClickProjectIntroModal} />
+      <S.IntroContent>
+        <IntroduceHeader />
+        <S.IntroduceBox>
+          <p>.</p>
+        </S.IntroduceBox>
+      </S.IntroContent>
     </modal>
   );
 };
