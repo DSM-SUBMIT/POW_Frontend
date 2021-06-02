0be1359e526148d39992f2c914f8f98f9aa769b4
kky04994 <kkyo8747@naver.com>
2021-06-02 19:58:58 +0900

    [ADD] <PostUploadModal.js> WhiteScreen 추가 후 모달 기능 추가

--- pow-front-v1/src/components/Club/modals/PostUpload/PostUploadModal.js
+++ pow-front-v1/src/components/Club/modals/PostUpload/PostUploadModal.js
@@ -1,44 +1,48 @@
 import React, { useState } from "react";
 import * as S from "../PostUpload/style";
-import GreyBox from "../common/GreyBox";
+import WhiteScreen from "../common/WhiteScreen";
 import "react-datepicker/dist/react-datepicker.css";
 
-const PostUploadModal = () => {
+const PostUploadModal = (props) => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+
+  const onClickPostUploadModal = () => {
+    props.setPostUploadModal(false);
+  };
+
   return (
     <modal>
-      <GreyBox>
-        <S.UpContent>
-          <S.Title>
-            <p>게시물 업로드</p>
-          </S.Title>
-          <S.UploadHeader>
-            <S.InputText placeholder="제목을 입력해 주세요" />
-            <S.DayBox>
-              <S.DayLable>시작일 : </S.DayLable>
-              <S.SelectDate
-                selected={startDate}
-                dateFormat="yyyy-MM-dd"
-                onChange={(date) => setStartDate(date)}
-                placeholder="클릭해 주세요."
-              />
-              <S.DayLable>종료일 : </S.DayLable>
-              <S.SelectDate
-                selected={endDate}
-                dateFormat="yyyy-MM-dd"
-                onChange={(date) => setEndDate(date)}
-                minDate={startDate}
-                placeholder="클릭해 주세요."
-              />
-            </S.DayBox>
-          </S.UploadHeader>
-          <S.WriteBox>
-            <S.InputContent placeholder="내용을 입력해 주세요" />
-          </S.WriteBox>
-          <S.UploadButton>업로드 하기</S.UploadButton>
-        </S.UpContent>
-      </GreyBox>
+      <WhiteScreen onClick={onClickPostUploadModal} />
+      <S.UpContent>
+        <S.Title>
+          <p>게시물 업로드</p>
+        </S.Title>
+        <S.UploadHeader>
+          <S.InputText placeholder="제목을 입력해 주세요" />
+          <S.DayBox>
+            <S.DayLable>시작일 : </S.DayLable>
+            <S.SelectDate
+              selected={startDate}
+              dateFormat="yyyy-MM-dd"
+              onChange={(date) => setStartDate(date)}
+              placeholder="클릭해 주세요."
+            />
+            <S.DayLable>종료일 : </S.DayLable>
+            <S.SelectDate
+              selected={endDate}
+              dateFormat="yyyy-MM-dd"
+              onChange={(date) => setEndDate(date)}
+              minDate={startDate}
+              placeholder="클릭해 주세요."
+            />
+          </S.DayBox>
+        </S.UploadHeader>
+        <S.WriteBox>
+          <S.InputContent placeholder="내용을 입력해 주세요" />
+        </S.WriteBox>
+        <S.UploadButton>업로드 하기</S.UploadButton>
+      </S.UpContent>
     </modal>
   );
 };
