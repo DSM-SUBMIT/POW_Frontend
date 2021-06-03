1c57b2a7ec7212b58ced2e6fbefb39a1d2989609
kky04994 <kkyo8747@naver.com>
2021-06-03 18:33:23 +0900

    [ADD] <PostUploadModal.js> axios 기능 코드 추가

--- pow-front-v1/src/components/Club/modals/PostUpload/PostUploadModal.js
+++ pow-front-v1/src/components/Club/modals/PostUpload/PostUploadModal.js
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 import * as S from "../PostUpload/style";
 import WhiteScreen from "../common/WhiteScreen";
 import "react-datepicker/dist/react-datepicker.css";
+import { PostUpload } from "../../../Axios/Axios";
 
 const PostUploadModal = (props) => {
+  const [title, setTitle] = useState("");
+  const [content, setContnet] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHViX2lkIjoxfQ.HFXowaGXnoryNVa_SbW2TtkF8KzA9ZJDfX6OnXpR9_o`;
 
   const onClickPostUploadModal = () => {
     props.setPostUploadModal(false);
@@ -19,7 +23,13 @@ const PostUploadModal = (props) => {
           <p>게시물 업로드</p>
         </S.Title>
         <S.UploadHeader>
-          <S.InputText placeholder="제목을 입력해 주세요" />
+          <S.InputText
+            placeholder="제목을 입력해 주세요"
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
           <S.DayBox>
             <S.DayLable>시작일 : </S.DayLable>
             <S.SelectDate
@@ -39,9 +49,21 @@ const PostUploadModal = (props) => {
           </S.DayBox>
         </S.UploadHeader>
         <S.WriteBox>
-          <S.InputContent placeholder="내용을 입력해 주세요" />
+          <S.InputContent
+            placeholder="내용을 입력해 주세요"
+            value={content}
+            onChange={(e) => {
+              setContnet(e.target.value);
+            }}
+          />
         </S.WriteBox>
-        <S.UploadButton>업로드 하기</S.UploadButton>
+        <S.UploadButton
+          onClick={(e) => {
+            PostUpload(title, content, startDate, endDate, 1, token);
+          }}
+        >
+          업로드 하기
+        </S.UploadButton>
       </S.UpContent>
     </modal>
   );
