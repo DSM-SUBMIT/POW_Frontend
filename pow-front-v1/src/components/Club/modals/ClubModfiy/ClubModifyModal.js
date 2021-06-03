e560a3e836af314d98e158422c630edfed55d8a2
kky04994 <kkyo8747@naver.com>
2021-06-03 18:31:28 +0900

    [ADD] <ClubModifyModal.js> axios 기능 추가

--- pow-front-v1/src/components/Club/modals/ClubModfiy/ClubModifyModal.js
+++ pow-front-v1/src/components/Club/modals/ClubModfiy/ClubModifyModal.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "../ClubModfiy/style";
 import WhiteScreen from "../common/WhiteScreen";
+import { ClubModify } from "../../../Axios/Axios";
 
 const ClubModifyModal = (props) => {
+  const [content, setContent] = useState("");
+  const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHViX2lkIjoxfQ.HFXowaGXnoryNVa_SbW2TtkF8KzA9ZJDfX6OnXpR9_o`;
   const onClickGreyBox = () => {
     props.setClubModifyModal(false);
   };
@@ -14,9 +17,21 @@ const ClubModifyModal = (props) => {
           <p>동아리 소개 수정</p>
         </S.Title>
         <S.WriteBox>
-          <S.InputContent placeholder="내용을 입력해 주세요" />
+          <S.InputContent
+            placeholder="내용을 입력해 주세요"
+            value={content}
+            onChange={(e) => {
+              setContent(e.target.value);
+            }}
+          />
         </S.WriteBox>
-        <S.UploadButton>업로드 하기</S.UploadButton>
+        <S.UploadButton
+          onClick={(e) => {
+            ClubModify(content, 1, token);
+          }}
+        >
+          업로드 하기
+        </S.UploadButton>
       </S.CmContent>
     </modal>
   );
