b5341a8e1d6dfddc0c4c911e0f58f07a37675c54
Kimjimin0807 <jm004471@naver.com>
2021-06-03 20:26:25 +0900

    [ADD]<BannerUpload.js>이미지만 선택 가능하도록 변경

--- pow-front-v1/src/components/Club/modals/BannerUpload/BannerUpload.js
+++ pow-front-v1/src/components/Club/modals/BannerUpload/BannerUpload.js
@@ -2,17 +2,16 @@ import React, { useState } from 'react';
 import * as s from './style';
 import { FileRequest } from '../../../Axios/Axios';
 
-const BannerUpload = (props) => {
+const BannerUpload = ({setBannerModal}) => {
   const [filePath, setFilePath] = useState('');
   const [file, setFile] = useState(null);
   const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHViX2lkIjoxLCJpYXQiOjE2MjIwMjU3ODV9.HtxbzxBBbA3-80WE1gP8sefqRoLC2DlBaAlyAX4xdzQ';
 
   const onClickWhiteScreen = () => {
-    props.setBannerModal(false);
+    setBannerModal(false);
   }
 
   const onChangeFile = (e) => {
-    console.log(e.target.files[0]);
     setFile(e.target.files[0]);
     let str = e.target.value.slice(12, e.target.value.length);
     setFilePath(str);
@@ -22,11 +21,12 @@ const BannerUpload = (props) => {
     try{
       const fd = new FormData();
       fd.append("file", file);
-      console.log(fd);
-      const {data} = FileRequest('PUT', 'banner/1', {
+      FileRequest('PUT', 'banner/1', {
         authorization: `Bearer ${token}`,
       }, fd);
-      console.log(data);
+      setBannerModal(false);
+      alert("배너 사진이 업로드 되었습니다.");
+
     } catch(e) {
       console.log(e);
     }
@@ -35,7 +35,7 @@ const BannerUpload = (props) => {
 
   return(
     <>
-      <s.WhiteScreen onClick={onClickWhiteScreen}></s.WhiteScreen>
+      <s.WhiteScreen onClick={onClickWhiteScreen}/>
       <s.Modal>
         <s.Title>
           <p>배너 사진 업로드</p>
@@ -43,7 +43,11 @@ const BannerUpload = (props) => {
         <s.FileInput>
           <s.FileBtn htmlFor="fileUpload" ><p>파일 선택</p></s.FileBtn>
           <s.FilePath htmlFor="fileUpload"><p>{filePath}</p></s.FilePath>
-          <s.FileUpload id="fileUpload" type="file" onChange={onChangeFile}></s.FileUpload>
+          <s.FileUpload 
+            id="fileUpload" 
+            type="file" 
+            accept="image/jpeg,image/png" 
+            onChange={onChangeFile}/>
         </s.FileInput>
         <s.UploadBtn onClick={onCLickUpload}>업로드 하기</s.UploadBtn>
       </s.Modal>
