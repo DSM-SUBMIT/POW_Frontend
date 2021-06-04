5a67b6777c11abf79b6fc51b55afaa1e3f50b18f
kky04994 <kkyo8747@naver.com>
2021-06-04 17:31:44 +0900

    [ADD] <PostUploadModal.js> 파일 경로 수정 후  props destructurion

--- pow-front-v1/src/components/Club/modals/PostUpload/PostUploadModal.js
+++ pow-front-v1/src/components/Club/modals/PostUpload/PostUploadModal.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import * as S from "../PostUpload/style";
+import * as S from "../postUpload/style";
 import WhiteScreen from "../common/WhiteScreen";
 import "react-datepicker/dist/react-datepicker.css";
-import { PostUpload } from "../../../Axios/Axios";
+import { PostUpload } from "../../../../axios/Axios";
 
 const PostUploadModal = (props) => {
+  const { setPostUploadModal } = props;
   const [title, setTitle] = useState("");
   const [content, setContnet] = useState("");
   const [startDate, setStartDate] = useState(new Date());
@@ -12,7 +13,7 @@ const PostUploadModal = (props) => {
   const token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHViX2lkIjoxfQ.HFXowaGXnoryNVa_SbW2TtkF8KzA9ZJDfX6OnXpR9_o`;
 
   const onClickPostUploadModal = () => {
-    props.setPostUploadModal(false);
+    setPostUploadModal(false);
   };
 
   return (
