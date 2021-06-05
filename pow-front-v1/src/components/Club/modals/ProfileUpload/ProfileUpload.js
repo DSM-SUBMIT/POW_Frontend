229abffc56a26ab351de8ce9ae9e2ed7d152dbe6
kky04994 <kkyo8747@naver.com>
2021-06-05 20:17:32 +0900

    [CHANGE] <ProfileUpload.js> 파일 경로 수정

--- pow-front-v1/src/components/Club/modals/ProfileUpload/ProfileUpload.js
+++ pow-front-v1/src/components/Club/modals/ProfileUpload/ProfileUpload.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
-import { FileRequest } from '../../../Axios/Axios';
-import * as s from './style';
+import React, { useState } from "react";
+import { FileRequest } from "../../../../axios/Axios";
+import * as s from "./style";
 
 const ProfileUpload = (props) => {
   const [file, setFile] = useState(null);
-  const [filePath, setFilePath] = useState('');
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHViX2lkIjoxLCJpYXQiOjE2MjIwMjU3ODV9.HtxbzxBBbA3-80WE1gP8sefqRoLC2DlBaAlyAX4xdzQ';
+  const [filePath, setFilePath] = useState("");
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbHViX2lkIjoxLCJpYXQiOjE2MjIwMjU3ODV9.HtxbzxBBbA3-80WE1gP8sefqRoLC2DlBaAlyAX4xdzQ";
 
   const onClickWhiteScreen = () => {
     props.setProfileModal(false);
@@ -16,22 +17,27 @@ const ProfileUpload = (props) => {
     setFile(e.target.files[0]);
     let str = e.target.value.slice(12, e.target.value.length);
     setFilePath(str);
-  }
+  };
 
-  const onSubmitFile = async() => {
-    try{
+  const onSubmitFile = async () => {
+    try {
       const fd = new FormData();
-      file&&fd.append("file", file);
-      const data = FileRequest('PUT', `banner/2`, {
-        authorization: `Bearer ${token}`,
-      }, fd);
+      file && fd.append("file", file);
+      const data = FileRequest(
+        "PUT",
+        `banner/2`,
+        {
+          authorization: `Bearer ${token}`,
+        },
+        fd
+      );
       console.log(data);
-    } catch(e) {
+    } catch (e) {
       console.log(e);
     }
-  } 
+  };
 
-  return(
+  return (
     <>
       <s.WhiteScreen onClick={onClickWhiteScreen}></s.WhiteScreen>
       <s.Modal>
