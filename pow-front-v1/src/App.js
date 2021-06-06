17e02d906d47553f3f1e77e3fd777d957bfc7855
Kimjimin0807 <jm004471@naver.com>
2021-06-06 11:10:09 +0900

    [CHANGE]<App.js> 경로 수정

--- pow-front-v1/src/App.js
+++ pow-front-v1/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Club from "./components/Club/Club";
-import Main from './components/Main/Main';
-import Header from "./components/Header/Header";
+import Club from "./components/club/Club";
+import Main from './components/main/Main';
+import Header from "./components/header/Header";
 import "./App.css";
 import { Route, BrowserRouter } from "react-router-dom";
 
