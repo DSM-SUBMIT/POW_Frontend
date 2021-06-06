6f7b4f4fb6ad4519ab0b858725f45edf66975299
Kimjimin0807 <jm004471@naver.com>
2021-06-06 11:08:53 +0900

    [CHANGE]<Style.js> 파일 분할

--- pow-front-v1/src/components/Club/modals/changepassword/Style.js
+++ pow-front-v1/src/components/Club/modals/changepassword/Style.js
@@ -56,55 +56,6 @@ export const Logo = styled.div`
     }
 `
 
-// Login Modal
-
-export const LoginModal = styled.div`
-    box-sizing: border-box;
-    width: 492px;
-    height: 492px;
-    border: 1px solid #707070;
-    padding: 10px 20px;
-    background-color: white;
-    z-index: 5;
-`
-
-export const Etc = styled.span`
-    display: flex;
-    justify-content: flex-end;
-    span {
-        font-size: 30px;
-        cursor: pointer;
-    }
-`
-
-export const LoginInput = styled.div`
-    width: 452px;
-    height: 176px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-around;
-`
-
-export const LoginBtn = styled.div`
-    height: 142px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`
-
-export const Change = styled.div`
-    color: #707070;
-    font-size: 10px;
-    padding-bottom: 5px;
-    :hover {
-        cursor: pointer;
-    }
-
-`
-
-// NewPw Modal
 export const PwModal = styled.div`
     width: 492px;
     height: 536px;
@@ -149,44 +100,4 @@ export const EnterBtn = styled.div`
     height: 120px;
     display: flex;
     align-items: center;
-`
-
-//account Delete Modal
-export const DelModal = styled.div`
-    width: 460px;
-    height: 400px;
-    box-sizing: border-box;
-    border: 1px solid #707070;
-    padding-top: 30px;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    background-color: white;
-    z-index: 5;
-    .accountCheck{
-        font-size: 16px;
-        color: #8D8D8D;
-    }
-`
-
-export const DelBtn = styled.div`
-    width: 250px;
-    height: 112px;
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    button {
-        width: 95px;
-        height: 59px;
-        color: white;
-        font-size: 15px;
-        border: none;
-        border-radius: 1em;
-    }
-    .agree {
-        background-color: #F4BEA1;
-    }
-    .cancel {
-        background-color: #BACFF3;
-    }
 `
