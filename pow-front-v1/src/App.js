361c21b3f7b635524b5c8e87852890c110df195c
yelim <choyr2016@naver.com>
2021-06-07 14:09:46 +0900

    .

--- pow-front-v1/src/App.js
+++ pow-front-v1/src/App.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import Header from "./components/Header/Header";
 import Main from './components/Main/Main';
-import AccountDel from './components/Modal/AccountDel';
 import { Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 
