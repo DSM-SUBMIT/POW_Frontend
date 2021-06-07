da28d4eda3b67663536c7e48482bb0bdb3d7b2cd
kky04994 <kkyo8747@naver.com>
2021-06-07 21:52:18 +0900

    [ADD] <App.js> club 파일 경로 추가

--- pow-front-v1/src/App.js
+++ pow-front-v1/src/App.js
@@ -2,6 +2,7 @@
 import React from "react";
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
+import Club from "./components/club/Club";
 import { Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 
