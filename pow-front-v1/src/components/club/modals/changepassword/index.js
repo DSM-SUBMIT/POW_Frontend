0075a6857c2ef1add8082bf27c84a1fdc1c0133a
yelim <choyr2016@naver.com>
2021-06-12 20:55:51 +0900

    [FEAT] <index.js> 비밀번호 변경

--- pow-front-v1/src/components/club/modals/changepassword/index.js
+++ pow-front-v1/src/components/club/modals/changepassword/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as s from './Style';
+import { changePw } from '../../../../axios/Axios';
 import pow from '../../../img/pow.png';
 
 const NewPw = ({setNewPwModal}) => {
-    const onClickEnter = () => {
+    const [password, setPassword] = useState();
+
+    const onClickEnter = async() => {
+        const { data } = await changePw(password);
+        const token = data['access-token'];
+        
         alert("성공 ..!");
     }
     const onClickAway = () => {
@@ -22,11 +28,29 @@ const NewPw = ({setNewPwModal}) => {
                         <span>비밀번호 변경</span>
                     </s.PwSpan>
                     <s.PwInput>
-                        <input className="ModalInput" placeholder="현재 비밀번호를 입력해주세요"></input>
+                        <input
+                            onChange={(event) => {
+                                setPassword(event.target.value);
+                            }}
+                            value={password}
+                            className="ModalInput"
+                            placeholder="현재 비밀번호를 입력해주세요"/>
                     </s.PwInput>
                     <s.NewPwInput>
-                        <input className="ModalInput" placeholder="변경할 비밀번호를 입력해주세요"></input>
-                        <input className="ModalInput" placeholder="비밀번호 확인"></input>
+                        <input
+                            onChange={(event) => {
+                                setPassword(event.target.value);
+                            }}
+                            value={password}
+                            className="ModalInput"
+                            placeholder="변경할 비밀번호를 입력해주세요"/>
+                        <input
+                            onChange={(event) => {
+                                setPassword(event.target.value);
+                            }}
+                            value={password}
+                            className="ModalInput"
+                            placeholder="비밀번호 확인"/>
                     </s.NewPwInput>
                     <s.EnterBtn>
                         <button onClick={onClickEnter} className="LoginBtn" >ENTER</button>
