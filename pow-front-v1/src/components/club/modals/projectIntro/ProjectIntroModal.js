0dd744883f419a21eb2b90b6e9b283ba7a97cef1
kky04994 <kkyo8747@naver.com>
2021-06-13 23:49:13 +0900

    [ADD] <ProjectIntroModal.js> 연동에 필요한 코드 추가

--- pow-front-v1/src/components/club/modals/projectIntro/ProjectIntroModal.js
+++ pow-front-v1/src/components/club/modals/projectIntro/ProjectIntroModal.js
@@ -2,8 +2,31 @@ import React from "react";
 import * as S from "./style";
 import WhiteScreen from "../common/WhiteScreen";
 import IntroduceHeader from "./IntroduceHeader";
+import { ProjectIntro } from "../../../../axios/Axios";
+import { useState, useEffect } from "react";
 
-const ProjectlntroModal = ({ closeModal }) => {
+const ProjectlntroModal = ({ closeModal, projectId }) => {
+  const [contents, setContents] = useState("");
+  const [writeDate, setWriteDate] = useState("");
+  const [modifyDate, setModifyDate] = useState("");
+  const [title, setTitle] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  useEffect(() => {
+    LoadProjectIntro();
+  }, []);
+  const LoadProjectIntro = async () => {
+    const res = await ProjectIntro(1, projectId);
+    console.log(res.data);
+    if (res) {
+      setContents(res.data.contents);
+      setTitle(res.data.title);
+      setWriteDate(res.data.created);
+      setModifyDate(res.data.updated_at);
+      setStartDate(res.data.started_at);
+      setEndDate(res.data);
+    }
+  };
   const onClickProjectIntroModal = () => {
     closeModal(null);
   };
@@ -12,10 +35,14 @@ const ProjectlntroModal = ({ closeModal }) => {
     <>
       <WhiteScreen onClick={onClickProjectIntroModal} />
       <S.IntroContent>
-        <IntroduceHeader />
-        <S.IntroduceBox>
-          <p>.</p>
-        </S.IntroduceBox>
+        <IntroduceHeader
+          writeDate={writeDate}
+          modifyDate={modifyDate}
+          title={title}
+          startDate={startDate}
+          endDate={endDate}
+        />
+        <S.IntroduceBox>{contents}</S.IntroduceBox>
       </S.IntroContent>
     </>
   );
