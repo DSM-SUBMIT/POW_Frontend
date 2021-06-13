78568c3fc881d06714c5678ff169f2fb13407142
yelim <choyr2016@naver.com>
2021-06-13 23:32:31 +0900

    [FEAT] <Main.js> 메인페이지 보기 추가

--- pow-front-v1/src/components/main/Main.js
+++ pow-front-v1/src/components/main/Main.js
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getAccountPicture } from '../../axios/Axios';
 import * as s from './style';
-import rosie from '../img/rosie.JPG';
 
 const Main = () => {
+    const IMG_BASEURL="https://ehddkfl.herokuapp.com";
+    const [data, setData] = useState();
+
+    useEffect(()  => {
+        getAccountPicture().then((res) => {
+            setData(res.data.clubs)
+        }).catch((err) => {
+            console.log(err)
+        })
+    }, [])
+    // console.log(data[0].profilePath)
     return(
         <s.Main>
             <s.Center>
-                <s.Club>
-                    <img src={rosie} alt=""/>
-                </s.Club>
-                <s.Club/>
-                <s.Club/>
-                <s.Club/>
-                <s.Club>
-                    <img src={rosie} alt=""/>
-                </s.Club>
-                <s.Club/>
-                <s.Club/>
-                <s.Club/><s.Club>
-                    <img src={rosie} alt=""/>
-                </s.Club>
-                <s.Club/>
-                <s.Club/>
-                <s.Club/>
+                {
+                    data && data.map((res) => {
+                        console.log(res)
+                        return (<s.Club key={res.name}>
+                        <img src={`${IMG_BASEURL}/public/profiles/${res.profilePath}`} alt=""/>
+                    </s.Club>)
+                    })
+                }
             </s.Center>
         </s.Main>
     )
