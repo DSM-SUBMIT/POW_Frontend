3503dd592cdbaa8653a9a6d01776df7473fda66a
Kimjimin0807 <jm004471@naver.com>
2021-06-29 23:00:31 +0900

    .

--- pow-front-v1/src/components/club/modals/profileDeleteModal/index.js
+++ pow-front-v1/src/components/club/modals/profileDeleteModal/index.js
@@ -20,6 +20,7 @@ const ProfileDeleteModal = ({closeModal, clubId}) => {
       },{}).then((e)=>{
         alert("프로필 사진이 초기화되었습니다.");
         closeModal(null);
+        window.location.reload();
       })
     } catch(e){
       alert("잠시 후 다시 시도해주세요.");
@@ -32,7 +33,7 @@ const ProfileDeleteModal = ({closeModal, clubId}) => {
       <WhiteScreen onClick={onClickWhiteScreen} />
       <S.DelContent>
         <S.Title>
-          <p>프로필 사진을 초기화합니다</p>
+          <p>프로필 사진을 초기화합니다.</p>
         </S.Title>
         <S.CheckButton onClick={onClickProfileDelete}>확인</S.CheckButton>
         <S.NoButton onClick={onClickCancelBtn}>취소</S.NoButton>
