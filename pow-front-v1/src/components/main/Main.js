7e6fbfeedca0b763adbfccd8ea3c5c87027e1fb7
Kimjimin0807 <jm004471@naver.com>
2021-06-30 16:47:05 +0900

    [CHANGE]<Main.js>코드 정리

--- pow-front-v1/src/components/main/Main.js
+++ pow-front-v1/src/components/main/Main.js
@@ -1,39 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import { getAccount } from '../../axios/Axios';
-import * as s from './style';
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { getAccount } from "../../axios/Axios";
+import * as s from "./style";
+
+const DEFAULTIMG =
+  "https://pow-bucket.s3.ap-northeast-2.amazonaws.com/1624976379907__asdsad.jpg";
 
 const Main = () => {
-    const IMG_BASEURL="https://ehddkfl.herokuapp.com";
-    const [data, setData] = useState();
-    const history = useHistory()
+  const [data, setData] = useState();
+  const history = useHistory();
 
-    const clubClick = (id) => {
-        history.push(`/club/${id}`)
-    }
+  const clubClick = (id) => {
+    history.push(`/club/${id}`);
+  };
 
-    useEffect(()  => {
-        getAccount().then((res) => {
-            setData(res.data.clubs)
-        }).catch((err) => {
-            console.log(err)
-        })
-    }, [])
-    // console.log(data[0].profilePath)
-    return(
-        <s.Main>
-            <s.Center>
-                {
-                    data && data.map((res, index) => {
-                        console.log(res)
-                        return (<s.Club onClick={() => clubClick(res.id)} key={index}>
-                        <img src={`${IMG_BASEURL}/public/profiles/${res.profile_path}`} alt=""/>
-                    </s.Club>)
-                    })
-                }
-            </s.Center>
-        </s.Main>
-    )
-}
+  useEffect(() => {
+    getAccount()
+      .then((res) => {
+        setData(res.data.clubs);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  // console.log(data[0].profilePath)
+  return (
+    <s.Main>
+      <s.Center>
+        {data &&
+          data.map((res, index) => {
+            console.log(res);
+            return (
+              <s.Club onClick={() => clubClick(res.id)} key={index}>
+                <img
+                  src={res.profile_path ? res.profile_path : DEFAULTIMG}
+                  alt=""
+                />
+              </s.Club>
+            );
+          })}
+      </s.Center>
+    </s.Main>
+  );
+};
 
 export default Main;
